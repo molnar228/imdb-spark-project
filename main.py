fde99e0ca8d138f5f6856d8620015fca91e67ddc
VolodymyrMolnar <redbag1337@gmail.com>
2026-03-04 10:18:28 +0200

    add_for_lab_2

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import os
 import sys
 import platform
+from pyspark.sql import SparkSession
 
 if platform.system() == "Windows":
     os.environ['PYSPARK_PYTHON'] = sys.executable
     os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
     os.environ['JAVA_HOME'] = r'C:\Progra~1\Java\jre1.8.0_51'
     os.environ['HADOOP_HOME'] = r'C:\hadoop'
-    print(">>> Запущено локально на Windows")
+    print(">>> Запуск локально (Windows)")
 else:
-
-    print(">>> Запущено всередині Docker-контейнера")
-
-from pyspark.sql import SparkSession
+    print(">>> Запуск у контейнері (Docker)")
 
 spark = SparkSession.builder \
-    .appName("IMDB_Project") \
+    .appName("TestDataFrameApp") \
     .getOrCreate()
 
-print("--- СИСТЕМА ПРАЦЮЄ! Spark версії:", spark.version, "---")
+test_data = [
+    ("The Shawshank Redemption", 1994, 9.3),
+    ("The Godfather", 1972, 9.2),
+    ("The Dark Knight", 2008, 9.0),
+    ("Pulp Fiction", 1994, 8.9)
+]
+
+columns = ["Movie_Title", "Release_Year", "IMDB_Rating"]
+
+df = spark.createDataFrame(test_data, schema=columns)
+
+print("\n--- ТЕСТОВИЙ DATAFRAME ---")
+df.show()
+
+df.printSchema()
 
 spark.stop()
